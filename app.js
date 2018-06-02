c0ffc04d38cd9a8d5130238d9f4541c2ad1b7479
Marius Kießling <github@mariuskiessling.de>
2018-06-02 17:45:17 +0200

    Fix max payload size (this time for real)

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '20mb'}));
+app.use(bodyParser.urlencoded({limit: '20mb', extended: true}));
 
 app.set('view engine', 'jade');
 
